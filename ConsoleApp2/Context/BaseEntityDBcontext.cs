a14e84c85dedaa56230f84d5dd628cb20824b946
Humbe_li59 <Humbe_li59@STHQ0126-13>
2024-09-03 14:59:38 +0400

    Add project files.

---
+++ ConsoleApp2/Context/BaseEntityDBcontext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+public class BaseEntityDBcontext:DbContext
+{
+    public DbSet<Products> Products { get; set; }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Data Source=STHQ0126-13;Initial Catalog = AlMarket;User ID=admin;Password=admin;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False");
+        base.OnConfiguring(optionsBuilder);
+    }
+}
